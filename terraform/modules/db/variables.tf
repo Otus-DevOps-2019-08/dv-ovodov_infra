095e52b935746464f0c97adfed7e9c9a481806e2
OvodovDV <dv.ovodoff@ya.ru>
2019-10-11 18:49:55 +0300

    HW7 Check modules

--- terraform/modules/db/variables.tf
+++ terraform/modules/db/variables.tf
@@ -1,16 +1,3 @@
-# variables.tf
-
-variable zone {
-  description = "europe-west3-c"
-  default = "europe-west3-c"
-}
-variable public_key_path {
-  description = "~/.ssh/odv.pub"
-}
-variable db_disk_image {
-  description = "Disk image for reddit db"
-  default = "reddit-db-base"
-}
 # db_variables.tf
 
 variable zone {
