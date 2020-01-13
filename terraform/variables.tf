17c682e4a9bfcd542569ca537d9c515f62f2e0a0
OvodovDV <dv.ovodoff@ya.ru>
2020-01-13 19:58:03 +0300

    HW7 Fix travis err, add storage-bucket module

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+# variables
+
+variable project {
+  # Описание переменной
+  description = "infra-253215"
+}
+variable region {
+  # Описание переменной
+  description = "europe-west3"
+  # Значение по умолчанию
+  default = "europe-west3"
+}
